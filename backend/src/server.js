a9e4a29625a43b81ac50f4298d05be5450c96b53
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2025-05-05 12:49:19 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,9 +36,9 @@ mongoose.connect(process.env.MONGODB_URI, {
 .catch((err) => console.error('MongoDB connection error:', err));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
 
